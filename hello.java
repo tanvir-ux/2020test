4324a686721b13bfca83d747d9942c062bb52e7c
tanvir-ux <tanviralamshuvo@gmail.com>
2020-01-05 00:58:41 +0600

    hello 2020 i am back

---
+++ hello.java
@@ -0,0 +1,21 @@
+class binarySearchTree{
+    private node root;
+    private node leftChild;
+    private node rightChild;
+
+    constructor(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public void delete(int index){
+
+    }
+
+    public node search(){
+
+    }
+}
